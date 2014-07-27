f89bd969da25fb455f95fb993a15ecbb64b8ae1b
Tim Brooks <tim.brooks@staples.com>
2014-07-27 13:09:15 -0700

    Committing plot 1 and source

--- plot1.R
+++ plot1.R
@@ -1,6 +1,21 @@
 
+create_plot <- function(data_list) {
+  par(las=2)
+  par(mar=c(5,8,4,2))
+  png(filename="plot1.png", width=480, height=480)
+  barplot(byYear$x, main="Emissions By Year", names.arg=byYear$year, cex.names=0.8)
+  dev.off()
+}
+
+aggregateOnYear <- function(data_frame) {
+  aggregate(data_frame$Emissions, by=list(year=data_frame$year), FUN=sum)  
+}
 
 readData <- function() {
-  NEI <- readRDS("summarySCC_PM25.rds")
-  SCC <- readRDS("Source_Classification_Code.rds")
+  NEI <- readRDS("exdata-data-NEI_data/summarySCC_PM25.rds")
+  NEI$year <- as.factor(NEI$year)
+}
+
+runScript <- function() {
+  create_plot(aggregateOnYear(readData()))
 }
